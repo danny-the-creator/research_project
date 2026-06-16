1ce3fa7bc9ce4de8e39b6e54cd67d62415601caa
danil <danilbadarev@gmail.com>
2026-06-16 17:05:21 +0200

    loading_model.py bugfix

--- tests/loading_model.py
+++ tests/loading_model.py
@@ -6,11 +6,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 LOCAL_PATH_LORA = "../saved_models/lora"
 LOCAL_PATH_SEFT = "../saved_models/seft"
 
-USE_SEFT = True
+USE_SEFT = False
 
 
 def load_latest(use_seft=USE_SEFT):
-    latest_model = os.path.join(LOCAL_PATH_LORA, os.listdir(LOCAL_PATH_LORA)[-1])
+    model_path = LOCAL_PATH_SEFT if use_seft else LOCAL_PATH_LORA
+    latest_model = os.path.join(model_path, os.listdir(LOCAL_PATH_LORA)[-1])
+    # print(model_path)
 
     # bnb_config = BitsAndBytesConfig(
     #     load_in_4bit=True,
@@ -48,8 +50,8 @@ if __name__ == '__main__':
         model=model,
         tokenizer=tokenizer,
         max_new_tokens=150,
-        temperature=0.7,
-        top_p=0.9,
+        # temperature=0.7,
+        # top_p=0.9,
         eos_token_id=terminators,
         pad_token_id=tokenizer.eos_token_id,
     )
--- tests/showcase.py
+++ tests/showcase.py
@@ -6,13 +6,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 LOCAL_PATH_LORA = "../saved_models/lora"
 LOCAL_PATH_SEFT = "../saved_models/seft"
 
+USE_SEFT = False
 
-def load_latest():
-    latest_lora = os.path.join(LOCAL_PATH_LORA, os.listdir(LOCAL_PATH_LORA)[-1])
-    latest_seft = os.path.join(LOCAL_PATH_SEFT, os.listdir(LOCAL_PATH_SEFT)[-1])
 
-    model_lora, tokenizer_lora = None, None
-    model_seft, tokenizer_seft = None, None
+def load_latest(use_seft=USE_SEFT):
+    model_path = LOCAL_PATH_SEFT if use_seft else LOCAL_PATH_LORA
+    latest_model = os.path.join(model_path, os.listdir(LOCAL_PATH_LORA)[-1])
+    # print(model_path)
 
     # bnb_config = BitsAndBytesConfig(
     #     load_in_4bit=True,
@@ -20,96 +20,63 @@ def load_latest():
     #     bnb_4bit_compute_dtype=torch.float16,
     #     bnb_4bit_use_double_quant=False,
     # )
-    #
-    #
-    # model_lora = AutoModelForCausalLM.from_pretrained(
-    #     latest_lora,
-    #     quantization_config=bnb_config,
-    #     device_map="auto",
-    # )
-    # tokenizer_lora = AutoTokenizer.from_pretrained(latest_lora)
 
-    model_seft = AutoModelForCausalLM.from_pretrained(
-        latest_seft,
+    model = AutoModelForCausalLM.from_pretrained(
+        latest_model,
         # quantization_config=bnb_config,
         torch_dtype=torch.bfloat16,
         device_map="auto",
     )
-    tokenizer_seft = AutoTokenizer.from_pretrained(latest_seft)
+    tokenizer = AutoTokenizer.from_pretrained(latest_model)
 
-    return (model_lora, tokenizer_lora), (model_seft, tokenizer_seft)
+    return model, tokenizer
+
+if __name__ == '__main__':
+    model, tokenizer = load_latest()
+
+    # print(model)
+    # print(tokenizer)
 
 
-(model_lora, tokenizer_lora), (model_seft, tokenizer_seft) = load_latest()
-
-# print(model_lora)
-# print(model_seft)
-# print(tokenizer_seft)
-
-# terminators_lora = [
-#     tokenizer_lora.eos_token_id,
-#     tokenizer_lora.convert_tokens_to_ids("<|eot_id|>")
-# ]
-
-terminators_seft = [
-    tokenizer_seft.eos_token_id,
-    tokenizer_seft.convert_tokens_to_ids("<|eot_id|>")
-]
-
-# 2. Set up the simplified pipeline
-
-# LORA
-# generator = pipeline(
-#     "text-generation",
-#     model=model_lora,
-#     tokenizer=tokenizer_lora,
-#     max_new_tokens=150,
-#     temperature=0.7,   # Creative temperature
-#     top_p=0.9,
-#     eos_token_id=terminators_lora,
-#     pad_token_id=tokenizer_lora.eos_token_id,
-# )
-
-# SEFT
-generator = pipeline(
-    "text-generation",
-    model=model_seft,
-    tokenizer=tokenizer_seft,
-    max_new_tokens=150,
-    temperature=0.7,
-    top_p=0.9,
-    eos_token_id=terminators_seft,
-    pad_token_id=tokenizer_seft.eos_token_id,
-)
-
-# 3. Showcase prompt
-
-while True:
-    print("Type your question here:")
-    command = input(">>> ")
-    if command == "exit":
-        break
-
-    messages = [
-        {"role": "user", "content": f"{command}"},
+    terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
 
-    # formatted_prompt = tokenizer_lora.apply_chat_template(
-    #     messages,
-    #     tokenize=False,
-    #     add_generation_prompt=True
-    # )
+    # 2. Set up the simplified pipeline
 
-    formatted_prompt = tokenizer_seft.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
+    generator = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=150,
+        # temperature=0.7,
+        # top_p=0.9,
+        eos_token_id=terminators,
+        pad_token_id=tokenizer.eos_token_id,
     )
 
-    print("\n--- Generating  Text ---")
-    output = generator(formatted_prompt)
-    # Extract and print output text cleanly
-    generated_text = output[0]["generated_text"][len(formatted_prompt):]
-    print(generated_text.strip())
-    print("---------------------------------")
+    # 3. Showcase prompt
 
+    while True:
+        print("Type your question here:")
+        command = input(">>> ")
+        if command == "exit":
+            break
+
+        messages = [
+            {"role": "user", "content": f"{command}"},
+        ]
+
+        formatted_prompt = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+
+        print("\n--- Generating  Text ---")
+        output = generator(formatted_prompt)
+        # Extract and print output text cleanly
+        generated_text = output[0]["generated_text"][len(formatted_prompt):]
+        print(generated_text.strip())
+        print("---------------------------------")
