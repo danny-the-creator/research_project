bcd170948a78e0a10e738a67d8be4a0d497076c4
danil <danilbadarev@gmail.com>
2026-06-16 16:11:19 +0200

    simplified loading_model.py (loading one model selected in params, not two at the same time)

--- tests/loading_model.py
+++ tests/loading_model.py
@@ -6,110 +6,75 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 LOCAL_PATH_LORA = "../saved_models/lora"
 LOCAL_PATH_SEFT = "../saved_models/seft"
 
-def load_latest():
-    latest_lora = os.path.join(LOCAL_PATH_LORA, os.listdir(LOCAL_PATH_LORA)[-1])
-    latest_seft = os.path.join(LOCAL_PATH_SEFT, os.listdir(LOCAL_PATH_SEFT)[-1])
-
-    model_lora, tokenizer_lora = None, None
-    model_seft, tokenizer_seft = None, None
-
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
-        bnb_4bit_use_double_quant=False,
-    )
+USE_SEFT = True
 
 
-    model_lora = AutoModelForCausalLM.from_pretrained(
-        latest_lora,
-        quantization_config=bnb_config,
+def load_latest(use_seft=USE_SEFT):
+    latest_model = os.path.join(LOCAL_PATH_LORA, os.listdir(LOCAL_PATH_LORA)[-1])
+
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.float16,
+    #     bnb_4bit_use_double_quant=False,
+    # )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        latest_model,
+        # quantization_config=bnb_config,
+        torch_dtype=torch.bfloat16,
         device_map="auto",
     )
-    tokenizer_lora = AutoTokenizer.from_pretrained(latest_lora)
+    tokenizer = AutoTokenizer.from_pretrained(latest_model)
 
-    # model_seft = AutoModelForCausalLM.from_pretrained(
-    #     latest_seft,
-    #     # quantization_config=bnb_config,
-    #     torch_dtype=torch.bfloat16,
-    #     device_map="auto",
-    # )
-    # tokenizer_seft = AutoTokenizer.from_pretrained(latest_seft)
+    return model, tokenizer
 
-    return (model_lora, tokenizer_lora), (model_seft, tokenizer_seft)
+if __name__ == '__main__':
+    model, tokenizer = load_latest()
 
-(model_lora, tokenizer_lora), (model_seft, tokenizer_seft) = load_latest()
-
-# print(model_lora)
-# print(model_seft)
-# print(tokenizer_seft)
-
-terminators_lora = [
-    tokenizer_lora.eos_token_id,
-    tokenizer_lora.convert_tokens_to_ids("<|eot_id|>")
-]
-#
-# terminators_seft = [
-#     tokenizer_seft.eos_token_id,
-#     tokenizer_seft.convert_tokens_to_ids("<|eot_id|>")
-# ]
+    # print(model)
+    # print(tokenizer)
 
 
-# 2. Set up the simplified pipeline
-
-#LORA
-generator = pipeline(
-    "text-generation",
-    model=model_lora,
-    tokenizer=tokenizer_lora,
-    max_new_tokens=150,
-    temperature=0.7,   # Creative temperature
-    top_p=0.9,
-    eos_token_id=terminators_lora,
-    pad_token_id=tokenizer_lora.eos_token_id,
-)
-
-#SEFT
-# generator = pipeline(
-#     "text-generation",
-#     model=model_seft,
-#     tokenizer=tokenizer_seft,
-#     max_new_tokens=150,
-#     temperature=0.7,
-#     top_p=0.9,
-#     eos_token_id=terminators_seft,
-#     pad_token_id=tokenizer_seft.eos_token_id,
-# )
-
-# 3. Showcase prompt
-
-while True:
-    print("Type your question here:")
-    command = input(">>> ")
-    if command == "exit":
-        break
-
-    messages = [
-        {"role": "user", "content": f"{command}"},
+    terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
 
-    formatted_prompt = tokenizer_lora.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
+    # 2. Set up the simplified pipeline
+
+    generator = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=150,
+        temperature=0.7,
+        top_p=0.9,
+        eos_token_id=terminators,
+        pad_token_id=tokenizer.eos_token_id,
     )
 
-    # formatted_prompt = tokenizer_seft.apply_chat_template(
-    #     messages,
-    #     tokenize=False,
-    #     add_generation_prompt=True
-    # )
+    # 3. Showcase prompt
 
-    print("\n--- Generating  Text ---")
-    output = generator(formatted_prompt)
-    # Extract and print output text cleanly
-    generated_text = output[0]["generated_text"][len(formatted_prompt):]
-    print(generated_text.strip())
-    print("---------------------------------")
+    while True:
+        print("Type your question here:")
+        command = input(">>> ")
+        if command == "exit":
+            break
 
-    
+        messages = [
+            {"role": "user", "content": f"{command}"},
+        ]
+
+        formatted_prompt = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+
+        print("\n--- Generating  Text ---")
+        output = generator(formatted_prompt)
+        # Extract and print output text cleanly
+        generated_text = output[0]["generated_text"][len(formatted_prompt):]
+        print(generated_text.strip())
+        print("---------------------------------")
